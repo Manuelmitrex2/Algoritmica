9cd2bedab7fe7d749f4fb0599bb22be026a3b2b0
Manuel Fernando Mitre Bernal <24021719@uadeo.mx>
2024-09-08 15:29:49 -0700

    Add files via upload

---
+++ holamundo2.cxx
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    printf ("hola mundo");
+    printf("\n hola mundo");   //hola
+    printf("\nX     \nY");   //mundo
+    printf("\n      \n      hola munda");   //Manuel Mitre
+    printf("\n      \n          \"Hola mundo\"");
+}
